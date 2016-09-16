e7a121d25e41861608e9493133798539b5a05c0c
973243764@qq.com <xujun410250828>
2016-09-16 23:52:35 +0800

    修改项目名称

--- FunLibrary/src/main/java/com/xujun/funapp/view/main/fragment/NewsFragment.java
+++ FunLibrary/src/main/java/com/xujun/funapp/view/main/fragment/NewsFragment.java
@@ -1,11 +1,27 @@
 package com.xujun.funapp.view.main.fragment;
 
 import com.xujun.funapp.common.BindingBaseFragment;
+import com.xujun.funapp.common.mvp.BasePresenter;
+import com.xujun.funapp.databinding.FragemntNewsBinding;
 
 /**
  * @ explain:
  * @ author：xujun on 2016/9/16 23:47
  * @ email：gdutxiaoxu@163.com
  */
-public class NewsFragment extends BindingBaseFragment<> {
+public class NewsFragment extends BindingBaseFragment<FragemntNewsBinding, BasePresenter> {
+    @Override
+    protected int getContentViewLayoutID() {
+        return 0;
+    }
+
+    @Override
+    protected void initView(FragemntNewsBinding binding) {
+
+    }
+
+    @Override
+    protected BasePresenter setPresenter() {
+        return null;
+    }
 }
