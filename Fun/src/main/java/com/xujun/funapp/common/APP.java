a2d2b2372afc98473e229e3a05df5e82a14696ce
gdutxiaoxu <973243764@qq.com>
2016-11-18 23:15:04 +0800

    提供定位功能

--- Fun/src/main/java/com/xujun/funapp/common/APP.java
+++ Fun/src/main/java/com/xujun/funapp/common/APP.java
@@ -19,22 +19,31 @@ public class APP extends BaseApp {
     public LocationClient mLocationClient = null;
     public BDLocationListener myListener = new MyLocationListener();
 
-    private void initLocation(){
+    private void initLocation() {
+
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+
+
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
         );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
-        int span=1000;
+        int span = 1000;
         option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
         option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
         option.setOpenGps(true);//可选，默认false,设置是否使用gps
         option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
-        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
+        // .getLocationDescribe里得到，结果类似于“在北京天安门附近”
         option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
-        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.setIgnoreKillProcess(false);
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
         option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
         option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
         mLocationClient.setLocOption(option);
+
+        mLocationClient.start();
     }
 
     @Override
@@ -43,13 +52,11 @@ public class APP extends BaseApp {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         app = this;
 
-        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
-        mLocationClient.registerLocationListener( myListener );    //注册监听函数
-        WriteLogUtil.init(app);
-        initLocation();
-        mLocationClient.start();
-        // Thread.setDefaultUncaughtExceptionHandler(UnCatchExceptionHandler.getInstance());
 
+        WriteLogUtil.init(app);
+        //        initLocation();
+
+        // Thread.setDefaultUncaughtExceptionHandler(UnCatchExceptionHandler.getInstance());
 
 
     }
--- Fun/src/main/java/com/xujun/funapp/view/location/CityPickerActivity.java
+++ Fun/src/main/java/com/xujun/funapp/view/location/CityPickerActivity.java
@@ -14,16 +14,16 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.xujun.funapp.R;
 import com.xujun.funapp.adapters.CityListAdapter;
 import com.xujun.funapp.adapters.ResultListAdapter;
 import com.xujun.funapp.beans.City;
 import com.xujun.funapp.beans.LocateState;
-import com.xujun.funapp.common.util.StringUtils;
 import com.xujun.funapp.common.util.ToastUtils;
 import com.xujun.funapp.db.DBManager;
 import com.xujun.funapp.widget.SideLetterBar;
@@ -32,6 +32,7 @@ import java.util.List;
 
 /**
  * 博客地址：http://blog.csdn.net/gdutxiaoxu
+ *
  * @author xujun
  * @time 2016/11/18 22:15.
  */
@@ -52,7 +53,8 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
     private List<City> mAllCities;
     private DBManager dbManager;
 
-    private AMapLocationClient mLocationClient;
+    private LocationClient mLocationClient;
+    private LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,30 +67,116 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
     }
 
     private void initLocation() {
-        mLocationClient = new AMapLocationClient(this);
-        AMapLocationClientOption option = new AMapLocationClientOption();
-        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        option.setOnceLocation(true);
-        mLocationClient.setLocationOption(option);
-        mLocationClient.setLocationListener(new AMapLocationListener() {
-            @Override
-            public void onLocationChanged(AMapLocation aMapLocation) {
-                if (aMapLocation != null) {
-                    if (aMapLocation.getErrorCode() == 0) {
-                        String city = aMapLocation.getCity();
-                        String district = aMapLocation.getDistrict();
-                        Log.e("onLocationChanged", "city: " + city);
-                        Log.e("onLocationChanged", "district: " + district);
-                        String location = StringUtils.extractLocation(city, district);
-                        mCityAdapter.updateLocateState(LocateState.SUCCESS, location);
-                    } else {
-                        //定位失败
-                        mCityAdapter.updateLocateState(LocateState.FAILED, null);
-                    }
+
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationListener = new LocationListener();
+        mLocationClient.registerLocationListener(mLocationListener);    //注册监听函数
+
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
+        // .getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+        //        开始 定位
+        mLocationClient.start();
+
+
+    }
+
+    private boolean isSuccess(BDLocation bdLocation) {
+        return bdLocation.getLocType() == 61 || bdLocation.getLocType() == 65 || bdLocation
+                .getLocType() == 66
+                || bdLocation.getLocType() == 161;
+    }
+
+    private class LocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if (bdLocation != null) {
+                if (isSuccess(bdLocation)) {
+                    String city = bdLocation.getCity();
+                    mCityAdapter.updateLocateState(LocateState.SUCCESS, city);
+
+                } else {
+                    mCityAdapter.updateLocateState(LocateState.FAILED, null);
                 }
             }
-        });
-        mLocationClient.startLocation();
+
+            BDLocation location = bdLocation;
+
+            //Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());// 单位：公里每小时
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());// 单位度
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list = location.getPoiList();// POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+            Log.i("BaiduLocationApiDem", sb.toString());
+        }
     }
 
     private void initData() {
@@ -106,7 +194,7 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
             public void onLocateClick() {
                 Log.e("onLocateClick", "重新定位...");
                 mCityAdapter.updateLocateState(LocateState.LOCATING, null);
-                mLocationClient.startLocation();
+                mLocationClient.start();
             }
         });
 
@@ -131,10 +219,12 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
         searchBox = (EditText) findViewById(R.id.et_search);
         searchBox.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
 
             @Override
             public void afterTextChanged(Editable s) {
@@ -174,17 +264,17 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
         backBtn.setOnClickListener(this);
     }
 
-    private void back(String city){
+    private void back(String city) {
         ToastUtils.showToast(this, "点击的城市：" + city);
-//        Intent data = new Intent();
-//        data.putExtra(KEY_PICKED_CITY, city);
-//        setResult(RESULT_OK, data);
-//        finish();
+        //        Intent data = new Intent();
+        //        data.putExtra(KEY_PICKED_CITY, city);
+        //        setResult(RESULT_OK, data);
+        //        finish();
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.iv_search_clear:
                 searchBox.setText("");
                 clearBtn.setVisibility(View.GONE);
@@ -200,6 +290,7 @@ public class CityPickerActivity extends AppCompatActivity implements View.OnClic
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mLocationClient.stopLocation();
+        mLocationClient.stop();
+        mLocationClient.unRegisterLocationListener(mLocationListener);
     }
 }
